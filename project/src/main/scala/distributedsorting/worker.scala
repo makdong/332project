d14bdebc80917ccdd23221c8683078fb3a2ee793
yunseong0518 <yunseong0518@gmail.com>
2022-11-27 22:40:15 +0900

    [Refactoring] separate block

--- project/src/main/scala/distributedsorting/worker.scala
+++ project/src/main/scala/distributedsorting/worker.scala
@@ -1,11 +1,5 @@
 package distributedsorting
 
-case class Block(line: String) {
-    def key = line.substring(0, 10)
-    def num = line.substring(12, 44)
-    def str = line.substring(46, 98)
-}
-
 object worker {
     val unsortedBlocks: List[Block] = get()
 
