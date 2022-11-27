2db3f094e8c027bae11256128d719dfc927cbaae
makdong <dhkim0317@postech.ac.kr>
2022-11-27 21:23:55 +0900

    [Implement] initiallize master.scala

--- project/src/main/scala/distributedsorting/master.scala
+++ project/src/main/scala/distributedsorting/master.scala
@@ -1,5 +1,38 @@
 package distributedsorting
 
-object master {
+import org.apache.log4j.Logger
+object master extends App {
+  def isIPValid(strings: Array[String]): Boolean = {
+    false
+  }
 
+  val logger = Logger.getLogger(this.getClass.getName)
+  logger.info("main program starts")
+
+  if (args.isEmpty) {
+    logger.info("No arguments. Test mode with hard coded IP address starts.")
+    val masterPort = 1234
+    val workerPort = List(
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+    )
+    val IPaddress = "111.222.333.444"
+  } else {
+    logger.info("Get arguments.")
+
+    try {
+      assert(args.length != 1, "Args.length is 1. There is no worker's IP address. Stop the program.")
+      assert(isIPValid(args), "The format of IP address is invalid. Stop the program.")
+    } catch {
+      e => logger.error(e)
+    }
+  }
 }
