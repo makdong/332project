9dc9edd0c8d5f1bb95ecb03433fee5a1e9fbcd0e
makdong <dhkim0317@postech.ac.kr>
2022-12-02 01:20:23 +0900

    [Implement] master, worker

--- project/src/main/scala/distributedsorting/master.scala
+++ project/src/main/scala/distributedsorting/master.scala
@@ -1,30 +1,35 @@
 package distributedsorting
 
+import distributedsorting.connection.CheckWorkersRunningGrpc.CheckWorkersRunningBlockingStub
 import org.apache.log4j.Logger
-import io.grpc.{Server, ServerBuilder}
+import io.grpc._
 import distributedsorting.connection.{CheckWorkersRunningGrpc, ConnectionRequest, ConnectionRespond}
 import distributedsorting.distribute.{DistributeBlocksGrpc, DistributeRequest, DistributeRespond}
 import distributedsorting.shuffle.{GiveCriteriaGrpc, ShuffleRequest, ShuffleRespond}
 import distributedsorting.merge.{MergeBlockGrpc, MergeRequest, MergeRespond}
 
 import scala.concurrent.{ExecutionContext, Future}
+import java.util.concurrent.TimeUnit
+
+object Invalid extends Exception {}
 object master extends App {
+  main()
   def isIPValid(isPort: Boolean, ips: Array[String]): Boolean = {
-    try{
+    try {
       var ip_array = Array[String]()
       var port_array = Array[Int]()
-      var temp_ip:String = null
-      var temp_port:Int = null
-      for(ip <- strings) {
-        if(isPort) {
-          if(ip_array.isEmpty && port_array.isEmpty) {
+      var temp_ip: String = null
+      var temp_port: Int = -1
+      for (ip <- ips) {
+        if (isPort) {
+          if (ip_array.isEmpty && port_array.isEmpty) {
             temp_ip = ip.split(":").apply(0)
             temp_port = ip.split(":").apply(1).toInt
             var temp_ip_array = temp_ip.split(".")
-            if(temp_ip_array.length != 4){
+            if (temp_ip_array.length != 4) {
               throw Invalid
             }
-            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt >255) throw Invalid)
+            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt > 255) throw Invalid)
             ip_array +:= temp_ip
             port_array +:= temp_port
           }
@@ -32,85 +37,119 @@ object master extends App {
             temp_ip = ip.split(":").apply(0)
             temp_port = ip.split(":").apply(1).toInt
             var temp_ip_array = temp_ip.split(".")
-            if(temp_ip_array.length != 4){
+            if (temp_ip_array.length != 4) {
               throw Invalid
             }
-            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt >255) throw Invalid)
-            ip_array.foreach(past_ip => if(past_ip != temp_ip) throw Invalid)
-            port_array.foreach(past_port => if(past_port == temp_port) throw Invalid)
+            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt > 255) throw Invalid)
+            ip_array.foreach(past_ip => if (past_ip != temp_ip) throw Invalid)
+            port_array.foreach(past_port => if (past_port == temp_port) throw Invalid)
             port_array +:= temp_port
           }
-        } 
+        }
 
         else {
-          if(ip_array.isEmpty) {
+          if (ip_array.isEmpty) {
             var temp_ip_array = ip.split(".")
-            if(temp_ip_array.length != 4){
+            if (temp_ip_array.length != 4) {
               throw Invalid
             }
-            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt >255) throw Invalid)
+            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt > 255) throw Invalid)
             ip_array +:= temp_ip
           }
           else {
             var temp_ip_array = ip.split(".")
-            if(temp_ip_array.length != 4){
+            if (temp_ip_array.length != 4) {
               throw Invalid
             }
-            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt >255) throw Invalid)
-            ip_array.foreach(past_ip => if(past_ip == temp_ip) throw Invalid)
+            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt > 255) throw Invalid)
+            ip_array.foreach(past_ip => if (past_ip == temp_ip) throw Invalid)
             port_array +:= temp_port
           }
         }
       }
     }
-    catch{
-      case Invalid: false
+    catch {
+      case Invalid => {
+        false
+      }
     }
     true
   }
+  def main(): Unit = {
 
-  val logger = Logger.getLogger(this.getClass.getName)
-  logger.info("main program starts")
+    val logger = Logger.getLogger(this.getClass.getName)
+    logger.info("main program starts")
 
-  if (args.isEmpty) {
-    logger.info("No arguments. Test mode with hard coded IP address starts.")
-    val worker_num = 10
-    val masterPort = 8023
-    val workerPort = List(
-      "8024",
-      "8025",
-      "8026",
-      "8027",
-      "8028",
-      "8029",
-      "8030",
-      "8031",
-      "8032",
-      "8033",
-    )
-    val IPaddress = "141.223.232.63"
-  } else {
-    logger.info("Get arguments.")
+    val channel = ManagedChannelBuilder.forAddress("localhost", 9000).usePlaintext().build()
+    val blockingStub = CheckWorkersRunningGrpc.blockingStub(channel)
+
+    val master = new master(channel, blockingStub)
+
+    if (args.isEmpty) {
+      logger.info("No arguments. Test mode with hard coded IP address starts.")
+      val worker_num = 10
+      val masterPort = 8023
+      val workerPort = List(
+        "8024",
+        "8025",
+        "8026",
+        "8027",
+        "8028",
+        "8029",
+        "8030",
+        "8031",
+        "8032",
+        "8033",
+      )
+      val IPaddress = "141.223.232.63"
+    }
+    else {
+      logger.info("Get arguments.")
+
+      try {
+        assert(args.length != 1, "Args.length is 1. There is no worker's IP address. Stop the program.")
+
+        val args_length = args.length
+        if (args.apply(0) == "-p") {
+          val worker_num = args.apply(1).toInt
+          val ip_array = args.slice(2, args_length)
+          assert(isIPValid(true, ip_array), "The format of IP address is invalid. Stop the program.")
+          logger.info("IP Addresses and Port are Valid")
+        }
+        else {
+          val worker_num = args.apply(0).toInt
+          val ip_array = args.slice(1, args_length)
+          assert(isIPValid(false, ip_array), "The format of IP address is invalid. Stop the program.")
+          logger.info("IP Addresses and Port are Valid")
+        }
+      } catch {
+        case e : Throwable => logger.error(e)
+      }
+    }
 
     try {
-      assert(args.length != 1, "Args.length is 1. There is no worker's IP address. Stop the program.")
-      
-      val args_array = args.split(" ")
-      val args_length = args_array.length
-      if (args_array.apply(0) == "-p") then {
-        val worker_num = args_array.apply(1).toInt
-        val ip_array = args_arrry.slice(2,args_length)
-        assert(isIPValid(true, ip_array), "The format of IP address is invalid. Stop the program.")
-        logger.info("IP Addresses and Port are Valid")
-      }
-      else {
-        val worker_num = args_array.apply(0).toInt
-        val ip_array = args_arrry.slice(1,args_length)
-        assert(isIPValid(false, ip_array), "The format of IP address is invalid. Stop the program.")
-        logger.info("IP Addresses and Port are Valid")
-      }
-    } catch {
-      e => logger.error(e)
+      master.checkWorkersRunning()
+    } finally {
+      master.shutdown()
+    }
+  }
+}
+
+class master private(private val channel: ManagedChannel, private val blockingStub: CheckWorkersRunningBlockingStub) {
+  val logger = Logger.getLogger(this.getClass.getName)
+  def shutdown(): Unit = {
+    channel.shutdown.awaitTermination(5, TimeUnit.SECONDS)
+  }
+
+  def checkWorkersRunning(): Unit = {
+    val request = ConnectionRequest(isMasterStart = true)
+    try {
+      val response = blockingStub.connection(request)
+    }
+    catch {
+      case e: StatusRuntimeException =>
+//        logger.error("RPC failed", e)
+        logger.error("RPC failed: {0}", e)
     }
   }
 }
--- project/src/main/scala/distributedsorting/worker.scala
+++ project/src/main/scala/distributedsorting/worker.scala
@@ -1,5 +1,10 @@
 package distributedsorting
 
+import distributedsorting.connection.{CheckWorkersRunningGrpc, ConnectionRequest, ConnectionRespond}
+import io.grpc.{Server, ServerBuilder}
+
+import scala.concurrent.{ExecutionContext, Future}
+
 object worker {
     val unsortedBlocks: List[Block] = get()
 
@@ -16,5 +21,50 @@ object worker {
         val block1: Block = Block(line1)
         val block2: Block = Block(line2)
         List(block1, block2)
+    }
+
+    def main() = {
+
+        val server = new worker(ExecutionContext.global)
+        server.start()
+        server.blockUntilShutdown()
+    }
+}
+
+
+class worker(executionContext: ExecutionContext) { self =>
+    private[this] var server: Server = null
+
+    private def start(): Unit = {
+        server = ServerBuilder
+          .forPort(8023)
+          .addService(CheckWorkersRunningGrpc.bindService(new CheckWorkersRunning, ExecutionContext.global))
+          .build()
+          .start()
+
+        sys.addShutdownHook {
+            System.err.println("*** shutting down gRPC server since JVM is shutting down")
+            self.stop()
+            System.err.println("*** server shut down")
+        }
+    }
+
+    private def stop(): Unit = {
+        if (server != null) {
+            server.shutdown()
+        }
+    }
+
+    private def blockUntilShutdown(): Unit = {
+        if (server != null) {
+            server.awaitTermination()
+        }
+    }
+
+    private class CheckWorkersRunning extends CheckWorkersRunningGrpc.CheckWorkersRunning {
+        override def connection (req: ConnectionRequest) = {
+            val reply = ConnectionRespond(isWorkerStart = true)
+            Future.successful(reply)
+        }
     }
 }
