b73a9febde6968d649e09dfe1eec794047bd6b61
sjin9805 <s.jin.choi98@gmail.com>
2022-12-01 22:46:11 +0900

    [Implement] isIPValid Implementation Done

--- project/src/main/scala/distributedsorting/master.scala
+++ project/src/main/scala/distributedsorting/master.scala
@@ -2,8 +2,64 @@ package distributedsorting
 
 import org.apache.log4j.Logger
 object master extends App {
-  def isIPValid(isPort: Boolean, strings: Array[String]): Boolean = {
-    false
+  def isIPValid(isPort: Boolean, ips: Array[String]): Boolean = {
+    try{
+      var ip_array = Array[String]()
+      var port_array = Array[Int]()
+      var temp_ip:String = null
+      var temp_port:Int = null
+      for(ip <- strings) {
+        if(isPort) {
+          if(ip_array.isEmpty && port_array.isEmpty) {
+            temp_ip = ip.split(":").apply(0)
+            temp_port = ip.split(":").apply(1).toInt
+            var temp_ip_array = temp_ip.split(".")
+            if(temp_ip_array.length != 4){
+              throw Invalid
+            }
+            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt >255) throw Invalid)
+            ip_array +:= temp_ip
+            port_array +:= temp_port
+          }
+          else {
+            temp_ip = ip.split(":").apply(0)
+            temp_port = ip.split(":").apply(1).toInt
+            var temp_ip_array = temp_ip.split(".")
+            if(temp_ip_array.length != 4){
+              throw Invalid
+            }
+            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt >255) throw Invalid)
+            ip_array.foreach(past_ip => if(past_ip != temp_ip) throw Invalid)
+            port_array.foreach(past_port => if(past_port == temp_port) throw Invalid)
+            port_array +:= temp_port
+          }
+        } 
+
+        else {
+          if(ip_array.isEmpty) {
+            var temp_ip_array = ip.split(".")
+            if(temp_ip_array.length != 4){
+              throw Invalid
+            }
+            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt >255) throw Invalid)
+            ip_array +:= temp_ip
+          }
+          else {
+            var temp_ip_array = ip.split(".")
+            if(temp_ip_array.length != 4){
+              throw Invalid
+            }
+            temp_ip_array.foreach(ip_num => if (ip_num.toInt < 0 || ip_num.toInt >255) throw Invalid)
+            ip_array.foreach(past_ip => if(past_ip == temp_ip) throw Invalid)
+            port_array +:= temp_port
+          }
+        }
+      }
+    }
+    catch{
+      case Invalid: false
+    }
+    true
   }
 
   val logger = Logger.getLogger(this.getClass.getName)
@@ -35,13 +91,13 @@ object master extends App {
       val args_array = args.split(" ")
       val args_length = args_array.length
       if (args_array.apply(0) == "-p") then {
-        val worker_num = args_array.apply(1).toInt - 48
+        val worker_num = args_array.apply(1).toInt
         val ip_array = args_arrry.slice(2,args_length)
         assert(isIPValid(true, ip_array), "The format of IP address is invalid. Stop the program.")
         logger.info("IP Addresses and Port are Valid")
       }
       else {
-        val worker_num = args_array.apply(0).toInt - 48
+        val worker_num = args_array.apply(0).toInt
         val ip_array = args_arrry.slice(1,args_length)
         assert(isIPValid(false, ip_array), "The format of IP address is invalid. Stop the program.")
         logger.info("IP Addresses and Port are Valid")
