a2ff0480ee5232f7c234b44eabb3b9c9799145af
yunseong0518 <yunseong0518@gmail.com>
2022-12-03 23:46:04 +0900

    [Fix] master

--- project/src/main/scala/distributedsorting/master.scala
+++ project/src/main/scala/distributedsorting/master.scala
@@ -8,38 +8,38 @@ import distributedsorting.shuffle.{GiveCriteriaGrpc, ShuffleRequest, ShuffleResp
 import distributedsorting.merge.{MergeBlockGrpc, MergeRequest, MergeRespond}
 
 import scala.concurrent.{ExecutionContext, Future}
-// object master extends App {
-//   def isIPValid(strings: Array[String]): Boolean = {
-//     false
-//   }
+object master extends App {
+  def isIPValid(strings: Array[String]): Boolean = {
+    false
+  }
 
-//   val logger = Logger.getLogger(this.getClass.getName)
-//   logger.info("main program starts")
+  val logger = Logger.getLogger(this.getClass.getName)
+  logger.info("main program starts")
 
-//   if (args.isEmpty) {
-//     logger.info("No arguments. Test mode with hard coded IP address starts.")
-//     val masterPort = 1234
-//     val workerPort = List(
-//       "111",
-//       "111",
-//       "111",
-//       "111",
-//       "111",
-//       "111",
-//       "111",
-//       "111",
-//       "111",
-//       "111",
-//     )
-//     val IPaddress = "111.222.333.444"
-//   } else {
-//     logger.info("Get arguments.")
+  if (args.isEmpty) {
+    logger.info("No arguments. Test mode with hard coded IP address starts.")
+    val masterPort = 1234
+    val workerPort = List(
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+      "111",
+    )
+    val IPaddress = "111.222.333.444"
+  } else {
+    logger.info("Get arguments.")
 
-//     try {
-//       assert(args.length != 1, "Args.length is 1. There is no worker's IP address. Stop the program.")
-//       assert(isIPValid(args), "The format of IP address is invalid. Stop the program.")
-//     } catch {
-//       e => logger.error(e)
-//     }
-//   }
-// }
+    try {
+      assert(args.length != 1, "Args.length is 1. There is no worker's IP address. Stop the program.")
+      assert(isIPValid(args), "The format of IP address is invalid. Stop the program.")
+    } catch {
+      e => logger.error(e)
+    }
+  }
+}
