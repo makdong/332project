b3c7684e098b8aa597eeff51c1729c64f8484a96
yunseong0518 <yunseong0518@gmail.com>
2022-12-11 02:19:19 +0900

    merge

--- project/src/main/scala/distributedsorting/worker.scala
+++ project/src/main/scala/distributedsorting/worker.scala
@@ -18,4 +18,27 @@ object worker {
         def sortedBlock = TypeConverter.entryList2Block(sortedEntry)
         sortedBlock
     }
+
+    def merge(listOfBlock: List[List[Int]]) = {
+        val endValue = -1
+        def findMax(listOfBlock: List[Int]) = {
+            def line = listOfBlock.max
+            def idx = listOfBlock.lastIndexOf(line)
+            (idx, line)
+        }
+
+        def mergeRec(listOfBlock: List[List[Int]]) {
+            val listOfHead = listOfBlock.map(block => {
+                if (block.nonEmpty) block.head
+                else endValue
+            })
+            val max = findMax(listOfHead)
+
+            if (max._2 != endValue) {
+                println(max._2)
+                mergeRec(listOfBlock.updated(max._1, listOfBlock(max._1).tail))
+            }
+        }
+        mergeRec(listOfBlock)
+    }
 }
