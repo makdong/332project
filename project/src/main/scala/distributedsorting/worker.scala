7ec482d603e5da322bc49dbe70b3f0e0d669e924
yunseong0518 <yunseong0518@gmail.com>
2022-12-11 12:18:59 +0900

    [Implement] worker parsing

--- project/src/main/scala/distributedsorting/worker.scala
+++ project/src/main/scala/distributedsorting/worker.scala
@@ -3,31 +3,37 @@ package distributedsorting
 object worker extends App {
     def main(args: Array[String]): Unit = {
         val masterPort = args(0).toInt
-        def parsing(option: String, optionMap: Map[String, Int], argList: List[String]) = {
-            
-            match argList.head => {
-                case "-I" => {
-                    parsing("-I", optionMap, argList.tail)
-                }
-                case "-O" => {
-                    parsing("-I", optionMap, argList.tail)
-                }
-                case x: String => {
-                    match option {
-                        case "" => {
-                            parsing(option, optionMap ++ (option -> x), argList.tail)
-                        }
-                        case "-I" => {
-                            parsing("-I", optionMap, argList.tail)
-                        }
-                        case "-O" => {
-                            parsing("-O", optionMap, argList.tail)
+        def parsing(option: String, optionMap: [List[String], List[String]], argList: List[String]): [List[String], List[String]] = {
+            if (argList.size == 0) {
+                optionMap
+            } else {
+                match argList.head => {
+                    case "-I" => {
+                        parsing("-I", optionMap, argList.tail)
+                    }
+                    case "-O" => {
+                        parsing("-O", optionMap, argList.tail)
+                    }
+                    case x: String => {
+                        match option => {
+                            case "-I" => {
+                                parsing(option, [x :: optionMap._1, optionMap._2], argList.tail)
+                            }
+                            case "-O" => {
+                                parsing(option, [optionMap._1, x :: optionMap._2], argList.tail)
+                            }
+                            case _ => {
+                                print("check option")
+                                exit(0)
+                            }
                         }
                     }
                 }
             }
         }
 
-        parsing("", )
+        val optionMap = parsing("", [List(), List()], args.tail)
+        val inputDirectorys = optionMap._1
+        val outputDirectorys = optionMap._2
     }
 }
